1aff9eb95ff8fc24373a3bc4ec5d34c14fd55e5b
GiovanniCortes <giocortes19@gmail.com>
2023-06-27 18:27:44 -0400

    set up Firebase app project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import firebase from 'firebase'
+import 'firebase/firestore' // database
+import 'firebase/auth'      // user authentication
+
+// Firebase Hooks
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useCollectionData} from 'react-firebase-hooks/firestore'
+
+// Identify our project
+firebase.initializeApp({
+  apiKey: "AIzaSyBp5QdKwkon53jA3FAMDBhXfx1A7e46uwI",
+  authDomain: "huddleup-chat.firebaseapp.com",
+  projectId: "huddleup-chat",
+  storageBucket: "huddleup-chat.appspot.com",
+  messagingSenderId: "100997670425",
+  appId: "1:100997670425:web:afa70f4e99f3fbd217ac88",
+  measurementId: "G-ZKE09385WR"
+})
+
+// REFERECE FIREBASE SDK
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
 
